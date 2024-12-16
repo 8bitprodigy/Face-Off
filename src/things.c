739ed8c4b3fa09667e4c2a7c6f3880ceabe4f96a
8bitprodigy <chrisxdeboy@gmail.com>
2024-12-15 20:23:28 -0800

    Fixed DC Makefile.

--- src/things.c
+++ src/things.c
@@ -19,7 +19,9 @@ Actor_move(Actor *actor, float delta)
     Thing *thing = &actor->_;
     Vector2 new_position = Vector2Add(thing->position, Vector2Scale(actor->velocity, delta));
     printf("New Position>\t x: %.4f | y: %.4f\n", new_position.x, new_position.y); 
+    
     /* Todo... */
+    
     actor->prev_pos = thing->position;
     thing->position = new_position;
 }
